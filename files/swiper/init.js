6c24f4e93cb36175ef7498beb097efbc02fc4c70
Роман <69018645+Hetman-Novel@users.noreply.github.com>
2024-05-19 00:46:53 +0300

    Add files via upload

--- files/swiper/init.js
+++ files/swiper/init.js
@@ -67,3 +67,94 @@ if (reviewsSlider) {
    
    })
 }
+
+let pageProductWrapProductSliders = document.querySelector('.page-product__wrapProductSliders')
+if (pageProductWrapProductSliders) {
+   var swiper = new Swiper(".page-product__sliderSmall", {
+      spaceBetween: 17,
+      slidesPerView: 5,
+      slidesPerGroup: 1,
+      autoHeight: true,
+      //freeMode: true,
+      watchSlidesProgress: true,
+      watchOverflow: true,
+      loop: false,
+      //initialSlide: 1,
+      breakpoints: {
+         0: {
+            spaceBetween: 12,
+            slidesPerView: 3,
+         },
+         576: {
+            slidesPerView: 4,
+         },
+         768: {
+            slidesPerView: 5,
+            spaceBetween: 15,
+         },
+         801: {
+            slidesPerView: 5,
+            spaceBetween: 15,
+         },
+         1025: {
+            spaceBetween: 17,
+         },
+      },
+   });
+   var swiper2 = new Swiper(".page-product__sliderBig", {
+      effect: 'fade',
+      autoHeight: true,
+      fadeEffect: {
+         crossFade: true
+      },
+      thumbs: {
+         swiper: swiper,
+      },
+   });
+}
+
+let colorsAndShapesSlider = document.querySelector('.colors-and-shapes__slider')
+if (colorsAndShapesSlider) {
+   new Swiper(colorsAndShapesSlider, {  
+      slidesPerView: 4,
+      slidesPerGroup: 1,
+      spaceBetween: 9,
+      watchOverflow: true,
+      navigation: {
+         nextEl: '#colors-and-shapes-btn-next',
+         prevEl: '#colors-and-shapes-btn-prev',
+      },
+      effect: 'slide',
+      breakpoints: {
+         0: {
+            slidesPerView: 2,
+            spaceBetween: 12,
+         },
+         421: {
+            slidesPerView: 3,
+         },
+         576: {
+            slidesPerView: 4,
+         },
+         700: {
+            slidesPerView: 4,
+            spaceBetween: 15,
+         },
+         961: {
+            slidesPerView: 4,
+            spaceBetween: 9,
+         }
+      },
+   })
+}
+
+/*
+let slidersDisplayDelay = document.querySelectorAll('.slider-display-delay')
+if (slidersDisplayDelay) {
+   setTimeout(() => {
+      slidersDisplayDelay.forEach(function(sliderDisplayDelay) {
+         sliderDisplayDelay.classList.remove('display-delay')
+      })
+   }, 300);
+}
+*/
